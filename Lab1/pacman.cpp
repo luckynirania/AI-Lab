225b5fed07a32868954bf4cc05f483b3ad8aa9d6
luckynirania <luckynirania@gmail.com>
2020-01-09 18:10:53 +0530

    Adjacency list completed

--- Lab1/pacman.cpp
+++ Lab1/pacman.cpp
@@ -13,10 +13,10 @@ struct node{
 };
 
 struct adj{
-    vector<node> arr;
+    vector<node*> arr;
     void disp() {
         for(int i = 0;i < arr.size();i++) {
-            cout << arr[i].data << " ";
+            cout << arr[i]->data << " ";
         }
         cout << endl;
     }
@@ -34,29 +34,30 @@ int main() {
     int n = input[0].length();
     cout << m << " " << n << endl;
     adj adj_list[99999];
+    node graph[99999];
     for(int i = 1;i < m - 1;i++) {
         cout << input[i] << endl;
         for(int j = 1;j < n - 1;j++) {
             if(input[i][j] == ' ') {
                 adj temp;
                 if(input[i-1][j] == ' ') {
-                    node tmp;
-                    tmp.data = n*(i-1) + j;
+                    node* tmp = &graph[n*(i-1) + j];
+                    tmp->data = n*(i-1) + j;
                     temp.arr.push_back(tmp);
                 }
                 if(input[i+1][j] == ' ') {
-                    node tmp;
-                    tmp.data = n*(i+1) + j;
+                    node *tmp = &graph[n*(i+1) + j];
+                    tmp->data = n*(i+1) + j;
                     temp.arr.push_back(tmp);
                 }
                 if(input[i][j-1] == ' ') {
-                    node tmp;
-                    tmp.data = n*i + (j-1);
+                    node* tmp = &graph[n*i + (j-1)];
+                    tmp->data = n*i + (j-1);
                     temp.arr.push_back(tmp);
                 }
                 if(input[i][j+1] == ' ') {
-                    node tmp;
-                    tmp.data = n*i + (j+1);
+                    node* tmp = &graph[n*i + (j+1)];
+                    tmp->data = n*i + (j+1);
                     temp.arr.push_back(tmp);
                 }
                 adj_list[n*i + j] = temp;
