9ed3125df9d9cd2d378707f6bc15b3ae42897b09
luckynirania <luckynirania@gmail.com>
2020-01-13 18:55:09 +0530

    DFS Completed

--- Lab1/pacman.cpp
+++ Lab1/pacman.cpp
@@ -6,18 +6,19 @@
 #include <fstream>
 
 using namespace std;
-struct node{
+
+struct position {
     int data;
-    int depth;
+    position* parent;
     bool visited;
-    node() {
+    position() {
+        parent = NULL;
         visited = false;
-        depth = 0;
     }
 };
 
-struct adj{
-    vector<node*> arr;
+struct adj {
+    vector<position*> arr;
     void disp() {
         for(int i = 0;i < arr.size();i++) {
             cout << arr[i]->data << " ";
@@ -25,17 +26,6 @@ struct adj{
         cout << endl;
     }
 };
-
-void dfs(node* u,adj* adj_list,int dest,int &states) {
-    u->visited = true;
-    for(int i = 0;i < adj_list[u->data].arr.size();i++) {
-        if((u->data != dest) && adj_list[u->data].arr[i]->visited == false) {
-            states++;
-            adj_list[u->data].arr[i]->depth = u->depth + 1;
-            dfs(adj_list[u->data].arr[i], adj_list,dest,states);
-        }
-    }
-}
 
 int main() {
     ifstream inp;
@@ -47,151 +37,87 @@ int main() {
     while(getline(inp,temp)) {
         input.push_back(temp);
     }
+
     int m = input.size();
-    int n = input[0].length();     // each "input[i]"" is a string containing "+--+--+--+" or "|  +  |  "
-    // cout << m << " " << n << endl;
-    adj adj_list[99999];   // adj_list[3] contains sets of pointer to a "node" which are reachable from node "3"
-    node graph[99999];    // graph is set of actual nodes......note that adj_list contains pointer to these graph
-    input[0][0] = ' ';
-    for(int i = 0;i < m - 1;i++) {
-        // cout << input[i] << endl;
-        for(int j = 0;j < n - 1;j++) {
-            if(input[i][j] == ' ') {
-                adj temp;
-                if(input[i+1][j] == ' ' or input[i+1][j] == '*') {
-                    node *tmp = &graph[n*(i+1) + j];
-                    tmp->data = n*(i+1) + j;
-                    temp.arr.push_back(tmp);
-                }
-                if((i - 1 >= 0) && (input[i-1][j] == ' ' or input[i-1][j] == '*' )) {
-                    node* tmp = &graph[n*(i-1) + j];
-                    tmp->data = n*(i-1) + j;
-                    temp.arr.push_back(tmp);
-                }
-                if(input[i][j+1] == ' ' or input[i][j+1] == '*') {
-                    node* tmp = &graph[n*i + (j+1)];
-                    tmp->data = n*i + (j+1);
-                    temp.arr.push_back(tmp);
-                }
-                if((j - 1 >= 0) && (input[i][j-1] == ' ' or input[i][j-1] == '*' )) {
-                    node* tmp = &graph[n*i + (j-1)];
-                    tmp->data = n*i + (j-1);
-                    temp.arr.push_back(tmp);
-                }
-                adj_list[n*i + j] = temp;
-            }
-        }
-    }
-    // for(int i = 0;i < n*m;i++) {
-    //     cout << i << "\t";
-    //     adj_list[i].disp();
-    // }
+    int n = input[0].length();
+
     int source = 0;
-    int dest = n*(m - 1) - 1;
-    int states = 1;
+    int dest;
 
-    // implement bfs
-    if(algo == 0) {
-        queue<node*> que;
-        que.push(&graph[source]);
+    position node[99999];
+    adj adj_list[99999];
 
-        while(!que.empty()) { 
-            if(que.front()->data == dest) break;
-            node *tmp = que.front();
-            que.pop();
-            tmp->visited = true;
-            states++;
-            for(int i = 0;i < adj_list[tmp->data].arr.size();i++) {
-                if(adj_list[tmp->data].arr[i]->visited == false) {
-                    que.push(adj_list[tmp->data].arr[i]);
-                    adj_list[tmp->data].arr[i]->visited = true;
-                    adj_list[tmp->data].arr[i]->depth = tmp->depth + 1;
-                }
-            }
-        }
-    }
-
-    // implement dfs
-    if(algo == 1) {
-        stack <node*> stk;
-        stk.push(&graph[source]);
-
-        while(!stk.empty()) {
-            if(stk.top()->data == dest) break;
-            node* tmp = stk.top();
-            stk.pop();
-            tmp->visited = true;
-            states++;
-            for(int i = adj_list[tmp->data].arr.size() - 1;i >= 0;i--) {
-                if(adj_list[tmp->data].arr[i]->visited == false) {
-                    adj_list[tmp->data].arr[i]->visited = true;
-                    adj_list[tmp->data].arr[i]->depth = tmp->depth + 1;
-                    stk.push(adj_list[tmp->data].arr[i]);
-                }
-            }
-        }
-    }
-
-    // backtracking / path retracing (shortest obviously)
-    int number = dest;
-    int x,y;
-    while(input[1][1] != '0') {
-        x = number / n;
-        y = number % n;
-        input[x][y] = '0'; 
-        int dep = graph[number].depth - 1;
-        for(int i = 0;i < 4;i++) {
-            if(graph[number - 1].depth == dep) {
-                bool present = false;
-                for(int j = 0;j < adj_list[number - 1].arr.size();j++) {
-                    if(adj_list[number - 1].arr[j]->data == number) {
-                        present = true;
-                        number = number - 1;
-                        break;
-                    }
-                }
-            }
-            if(graph[number + 1].depth == dep) {
-                bool present = false;
-                for(int j = 0;j < adj_list[number + 1].arr.size();j++) {
-                    if(adj_list[number + 1].arr[j]->data == number) {
-                        present = true;
-                        number = number + 1;
-                        break;
-                    }
-                }
-            }
-            if(graph[number - n].depth == dep) {
-                bool present = false;
-                for(int j = 0;j < adj_list[number - n].arr.size();j++) {
-                    if(adj_list[number - n].arr[j]->data == number) {
-                        present = true;
-                        number = number - n;
-                        break;
-                    }
-                }
-            }
-            if(graph[number + n].depth == dep) {
-                bool present = false;
-                for(int j = 0;j < adj_list[number + n].arr.size();j++) {
-                    if(adj_list[number + n].arr[j]->data == number) {
-                        present = true;
-                        number = number + n;
-                        break;
-                    }
-                }
-            }
-        }
-    }
-    cout << "states explored\t" << states << endl;
-    cout << "length of path\t" << graph[dest].depth + 1 << endl; // + 1 bcoz counting from 0
-    
-    input[0][0] = '0';
-    input[1][0] = '0';
     for(int i = 0;i < input.size();i++) {
-        cout << input[i] << endl;
+        for(int j = 0;j < input[i].size();j++) {
+            int pos = n*i + j;
+            node[pos].data = pos;
+            if(input[i][j] == '*') dest = pos;
+            if(input[i][j] == ' ' or ( i == 0 && j == 0)) {
+                if(i < m && (input[i+1][j] == ' ' or input[i+1][j] == '*')) adj_list[pos].arr.push_back(&node[pos + n]);
+                if(i > 0 && (input[i-1][j] == ' ' or input[i-1][j] == '*')) adj_list[pos].arr.push_back(&node[pos - n]);
+                if(j < n && (input[i][j+1] == ' ' or input[i][j+1] == '*')) adj_list[pos].arr.push_back(&node[pos + 1]);
+                if(j > 0 && (input[i][j-1] == ' ' or input[i][j-1] == '*')) adj_list[pos].arr.push_back(&node[pos - 1]);
+            }
+        }
+    }
+    int states = 1;
+    int length = 1;
+
+    //BFS
+    if(algo == 0) {
+        queue<position*> que;
+        que.push(&node[source]);
+        
+        while(!que.empty()) {
+            int que_front_index = que.front()->data;
+            for(int i = 0;i < adj_list[que_front_index].arr.size();i++) {
+                int pos = adj_list[que_front_index].arr[i]->data;
+                if(node[pos].visited == false) {
+                    node[pos].visited = true;
+                    node[pos].parent = &node[que_front_index];
+                    que.push(&node[pos]);
+                }
+            }
+            if(que.front()->data == dest) break;
+            que.pop();
+            states++;
+        }
     }
 
+    //DFS
+    if(algo == 1) {
+        stack<position*> que;
+        que.push(&node[source]);
+        
+        while(!que.empty()) {
+            int que_front_index = que.top()->data;
+            que.pop();
+            states++;
+            for(int i = 0;i < adj_list[que_front_index].arr.size();i++) {
+                int pos = adj_list[que_front_index].arr[i]->data;
+                if(node[pos].visited == false) {
+                    node[pos].visited = true;
+                    node[pos].parent = &node[que_front_index];
+                    que.push(&node[pos]);
+                }
+            }
+            if(que.top()->data == dest) break;
+        }
+    }
+    if(algo == 2) {
 
-    return 0;
+    }
+
+    position* path = &node[dest];
+
+    while(path->parent != NULL) {
+        int x = path->data / n;
+        int y = path->data % n;
+        input[x][y] = '0';
+        path = path->parent;
+        length++;
+    }
+
+    cout << states << endl << length << endl;
+    for(int i = 0;i < input.size(); i++) cout << input[i] << endl;
 }
