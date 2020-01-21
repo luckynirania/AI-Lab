07cca8c4035f549c1f64e50a3789d52ffe9b32f9
SudarshaanDas <sudarshaandas31@gmail.com>
2020-01-21 21:52:10 +0530

    heuristic defined

--- Lab2/toh.py
+++ Lab2/toh.py
@@ -1,7 +1,10 @@
 import copy
+from queue import PriorityQueue
 class state:
-    childs = set()
+    cost = 0
+    childs = []
     poles = [[],[],[]]
+    visited = False
 
 def d_s(state):
     i = 0
@@ -16,15 +19,32 @@ def deepcopy(a):
     node.poles = copy.deepcopy(a.poles)
     return node
 
+def cost(state):
+    for i in range(0,3):
+        for j in state.poles[i]:
+            state.cost = state.cost + (i+1)*j
+
 adj_list = []
 
 node = state()
 
-discs = 3
+discs = 2
 ls = []
 for i in range(0,discs):
     ls.append(discs - i)
 node.poles = [ls,[],[]]
+cost(node)
+
+goal = state()
+ls = []
+for i in range(0,discs):
+    ls.append(discs - i)
+goal.poles = [[],[],ls]
+cost(goal)
+
+ref = goal.cost
+goal.cost = 0
+node.cost = ref - node.cost
 
 adj_list.append(node)
 
@@ -42,6 +62,8 @@ while(size != len(adj_list)):
                             temp = deepcopy(ref_state)
                             temp.poles[j].append(temp.poles[i][-1])
                             temp.poles[i].pop()
+                            cost(temp)
+                            temp.cost = ref - temp.cost
                             ind = -1
                             r = 0
                             for x in adj_list:
@@ -52,8 +74,16 @@ while(size != len(adj_list)):
                             if(ind < 0):
                                 ind = len(adj_list)
                                 adj_list.append(temp)
-                            ref_state.childs.add(ind)
+                            ref_state.childs.append(ind)
+        lok = set(ref_state.childs)
+        ref_state.childs = list(lok)
+
+for x in adj_list:
+    print("cost ",x.cost)
+    print(x.childs)
+    d_s(x)
 
 # for x in adj_list:
-#     print(x.childs)
-#     d_s(x)
+#     min = ref
+#     for i in range(len(x.childs)):
+        
